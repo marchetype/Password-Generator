4fe15230c8e7265b14c11a7506a7ad7b4dfe7dcb
marchetype <114883447+marchetype@users.noreply.github.com>
2022-11-10 19:25:48 -0500

    added final comments.

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,13 @@ let lengthInput;
 let chars = '';
 //let charsArray = chars.split(' ')
 
-//these are where parameters will be specified
+//these functions are used for parameters to be specified (character case, special and neumeric keys)
 
 function passwordLength() {
   lengthInput = prompt("password length:", "8-128 characters");
   //conditional statement needs to be made to check if there are not more or less than the specified amount of characters
     if (lengthInput < 8) {
+      //Unsure of how to bypass an invalid entry other than having a default password length.. if parameter specification is not 8-128 characters, then default length (12) will be stored in lengthInput variable.
       alert('specified length is not long enough!! default password length: 12');
       lengthInput = 12;
       console.log('password length: ' + lengthInput);
@@ -24,10 +25,11 @@ function passwordLength() {
 
 function lowercase() {
   let confirmLower = confirm("would you like to include lowercase letters in your password?");
- //conditional statement to determine wether lowercase letters need to be included in the password
+ //conditional statement to determine wether lowercase letters will be included in the password
     if (confirmLower) {
       //add lowercase letters
       chars = chars + 'abcdefghijklmnopqrstuvwxyz';
+      //console.log() commands used to help visualize JavaScript during development
       console.log('password string: ' + chars);
     } else {
       console.log('password string: ' + chars + ' |  lowercase letters not included in password string');
@@ -37,7 +39,7 @@ function lowercase() {
 
 function uppercase() {
   let confirmUpper = confirm("would you like to include uppercase letters in your password?");
-
+//same process used as before to determine inclusion of UPPERCASE characters
   if (confirmUpper) {
     chars = chars + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     console.log('password string: ' + chars);
@@ -67,38 +69,33 @@ function symbols() {
     console.log('password string: ' + chars + ' |  Symbols not included in password string');
   }
 }
-
+//creating individual functions above makes the function below much cleaner. also, the functions above will be accessible through other lines of code since they are not established inside the curly braces of function generatePassword().
 function generatePassword() {
   passwordLength();
   lowercase();
   uppercase();
   numerics();
   symbols();
-  //charsArray = chars.split(' ');
-  //console.log(charsArray);
- 
-  /* console.log(lengthInput);
-  console.log(confirmUpper);
-  console.log(confirmLower);
-  console.log(confirmNumeric);
-  console.log(confirmSymbol); */
 
-  let passwordValue = ''
+  let passwordValue = '';
+  // for iteration used to generate a random string of characters within the pre-specified parameters, included in variable: chars
   for (let i = 0; i <= lengthInput; i ++) {
     let passwordCharacter = Math.floor (Math.random() * chars.length);
    
-    passwordValue += chars.substring(passwordCharacter, passwordCharacter +1);
+    passwordValue = passwordValue + chars.substring(passwordCharacter, passwordCharacter +1);
   }
+
   console.log(passwordValue);
   console.log(chars);
   return passwordValue;
 }
 
+// function writePassword() calls generatePassword()
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
-  
   passwordText.value = password;
+  // if statement used below to notify user that they have not inputted any character types as parameters
   if (passwordText.value == '') {
     alert('No password created!! Please select at least one character type to include in generated password.');
   } else {
